84e55ddc002e0ed359e908456d5865fd06e31d5b
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-08-29 23:00:33 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,13 @@
 # code your #position_taken? method here!
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def position_taken?(board, index)
+  if
+    board[index] == " " || "" || nil
+    !true
+  elsif
+    board[index] != " " || "" || nil
+    true
+  end  
+end  
